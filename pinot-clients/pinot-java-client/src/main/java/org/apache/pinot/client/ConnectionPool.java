3c8772bfecde4bba10cd71327e020638aa3b13e6
Saurabh Dubey <saurabh.dubey@inmobi.com>
2022-02-08 15:54:17 +0530

    Add Connection pooling to java client

---
+++ pinot-clients/pinot-java-client/src/main/java/org/apache/pinot/client/ConnectionPool.java
@@ -0,0 +1,21 @@
+package org.apache.pinot.client;
+
+public class ConnectionPool extends ObjectPool<Connection> {
+    private final String zkUrl;
+
+    public ConnectionPool(int maxConnections, String zkUrl) throws InterruptedException {
+        super(maxConnections);
+        this.zkUrl = zkUrl;
+    }
+
+    @Override
+    Connection createObject() {
+        return ConnectionFactory.fromZookeeper(zkUrl);
+    }
+
+    @Override
+    boolean validate(Connection obj) {
+        // We could close this connection if certain validations fail (ttl expiry?) and return false
+        return true;
+    }
+}
---
+++ pinot-clients/pinot-java-client/src/main/java/org/apache/pinot/client/PinotHttpClient.java
@@ -0,0 +1,55 @@
+package org.apache.pinot.client;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class PinotHttpClient {
+
+    public static class PinotHttpClientConfig {
+        private int poolSize;
+        private String zkUrl;
+        private long connectionTimeOutMillis;
+
+        public int getPoolSize() {
+            return poolSize;
+        }
+
+        public void setPoolSize(int poolSize) {
+            this.poolSize = poolSize;
+        }
+
+        public String getZkUrl() {
+            return zkUrl;
+        }
+
+        public void setZkUrl(String zkUrl) {
+            this.zkUrl = zkUrl;
+        }
+
+        public long getConnectionTimeOutMillis() {
+            return connectionTimeOutMillis;
+        }
+
+        public void setConnectionTimeOutMillis(long connectionTimeOutMillis) {
+            this.connectionTimeOutMillis = connectionTimeOutMillis;
+        }
+    }
+    private PinotHttpClientConfig _pinotHttpClientConfig;
+    private ConnectionPool _connectionPool;
+
+
+    public PinotHttpClient(PinotHttpClientConfig config) throws InterruptedException {
+        this._pinotHttpClientConfig = config;
+        this._connectionPool = new ConnectionPool(config.poolSize, config.zkUrl);
+        _connectionPool.init();
+    }
+
+    public ResultSetGroup execute(String query) throws TimeoutException, InterruptedException {
+        Connection connection = _connectionPool.lease(_pinotHttpClientConfig.connectionTimeOutMillis, TimeUnit.MILLISECONDS);
+        Request request = new Request("sql", query);
+        ResultSetGroup result = connection.execute(request);
+        _connectionPool.release(connection);
+        return result;
+    }
+
+}
